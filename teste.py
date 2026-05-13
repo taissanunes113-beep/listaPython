c53a58098d7c3830ef8d197d279ad81f9eb5a223
taissanunes113-beep <taissanunes113@gmail.com>
2026-05-13 14:19:18 +0000

    v3.0

--- teste.py
+++ teste.py
@@ -1,1 +1,46 @@
+# pergunta 1
+print("/n1) qual e o maoir planeta do sistema solar?")
+print("a) terra")
+print("b) jupiter)
+print("c) marte")
+      
+resposta = input("digite a arternativa correta: ")
+if resposta.lower() == "b":
+print("voce acertou!")
+pontos+=1
+else:
+print(" voce errou!")
+
+#pergunta 2
+print("/n2) qual planeta e conhecido como planeta vermelho?")
+print("a) marte")
+print("b) saturno")
+print("c) venus")
+resposta = input("digite a alternativa correta: ")
+
+if resposta.lower() == "a":
+  print("voce acertou!")
+  pontos +=1
+else:
+  print(" voce errou!")
+
+#pergunta 3
+print("/n3) qual a estrela esta no centro do sistema solar: ")
+print("a) lua")
+print("b) sirius")
+print("c) sol")
+
+resposta = input("Digite a Alternativa Correta:  ")
+
+if resposta.lower() == "c":
+  print("Você Acertou!")
+  ponto += 1
+else:
+  print(f" \n Você fez {pontos} ponto(s)!")
+# Taissa Nunes dos santos 
+
+
+
+
+print("voce errou!")
 
